7f49e60d64ab4414ca1cf72877baa9a303b7f913
JB <jon@thebartletts.org>
2025-06-19 18:03:04 +0100

    Update __init__.py

--- custom_components/eon_next/__init__.py
+++ custom_components/eon_next/__init__.py
@@ -33,7 +33,7 @@ async def async_setup_entry(hass, entry):
     else:
         return False
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_unload_entry(hass, entry) -> bool:
     """Unload a config entry."""
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
         hass.data[DOMAIN].pop(entry.entry_id)
